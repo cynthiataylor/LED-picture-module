c9a7abb3e3467b42369392c2bf4d2ae7bb1a97ce
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-17 12:39:46 -0500

    New

--- Flower.py
+++ Flower.py
@@ -6,14 +6,11 @@ picture.new_picture(canvas, canvas)
 
 picture.draw_text(10, 2, "EXIT", font_size=1)
 
-picture.draw_text((10, 2), "EXIT", 1)
-
 beep = True
 while beep:
     picture.set_fill_color("red")
     for i in range(30):
         picture.draw_filled_circle(32, 40, 27)
-        time.sleep(0.1)
 
 #cvf
 picture.draw_on_matrix()
