267e93da392bbfc46d11df1581d745a206156ae7
Kelig Ngabo <144262857+kngabo@users.noreply.github.com>
2024-01-17 11:47:33 -0500

    Create Flower.py

---
+++ Flower.py
@@ -0,0 +1,23 @@
+import picture
+import time
+import keyboard
+
+canvas = 64
+picture.new_picture(canvas, canvas)
+
+def stop_beep(_): #You need to test this one
+    global beep
+    beep = False
+
+keyboard.add_hotkey("Ctrl+C", stop_beep)
+
+picture.draw_text((10, 2), "EXIT", font="Arial.ttf", fill="red", width=picture.PEN_WIDTH)
+
+beep = True
+while beep:
+    for i in range(30):
+        picture.draw_filled_circle((32, 40, 27), fill="red", outline = "red")
+        time.sleep(0.1)
+
+
+picture.draw_on_matrix()
