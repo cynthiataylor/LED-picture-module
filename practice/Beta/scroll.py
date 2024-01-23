6e50d908d160eb8afd30b3b5a34decd601c6d87f
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-23 09:43:11 -0500

    save

--- practice/Beta/scroll.py
+++ practice/Beta/scroll.py
@@ -1,10 +1,17 @@
+from folders import importing_library_picture_from_scroll_folder
+
+importing_library_picture_from_scroll_folder() # to avoid repetition of files in the same folder
+
+from Module.picture import *
+
+
 from picture import *
 import random
 import time
 
-new_picture(800,800)
+new_picture(128,128)
 set_fill_color("black")
-draw_filled_rectangle(0,0,800,800)
+draw_filled_rectangle(0,0,128,128)
 
 
 # set_outline_color("white")
@@ -22,7 +29,7 @@ count = 0
 while x < 1000:
     # for k in range(7):
     set_fill_color(red,green,blue)
-    y = random.randint(4,400)
+    y = random.randint(4,128)
     count += 1
     x += 1
     positions.append((x,y))
@@ -45,7 +52,8 @@ while x < 1000:
     green = (blue + 20) % 255
 
 
-    display()
+    # display()
+    draw_on_matrix()
     # time.sleep(1)
 
     
