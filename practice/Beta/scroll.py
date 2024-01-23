27c6e5eb3b9da143eae816b9da5008a0e8e743e2
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-23 09:53:01 -0500

    sace

--- practice/Beta/scroll.py
+++ practice/Beta/scroll.py
@@ -1,17 +1,12 @@
-from folders import importing_library_picture_from_scroll_folder
-
-importing_library_picture_from_scroll_folder() # to avoid repetition of files in the same folder
-
-from Module.picture import *
 
 
-from picture import *
+import picture
 import random
 import time
 
-new_picture(128,128)
-set_fill_color("black")
-draw_filled_rectangle(0,0,128,128)
+picture.new_picture(128,128)
+picture.set_fill_color("black")
+picture.draw_filled_rectangle(0,0,128,128)
 
 
 # set_outline_color("white")
@@ -28,7 +23,7 @@ positions = []
 count = 0
 while x < 1000:
     # for k in range(7):
-    set_fill_color(red,green,blue)
+    picture.set_fill_color(red,green,blue)
     y = random.randint(4,128)
     count += 1
     x += 1
@@ -41,10 +36,10 @@ while x < 1000:
             x = 0
         elif count % 9 == 0:
             y += 2
-        set_fill_color('black')
-        draw_filled_rectangle(0,0,800,800)
+        picture.set_fill_color('black')
+        picture.draw_filled_rectangle(0,0,800,800)
 
-    draw_filled_square((x) * (600/12),y,50)
+    picture.draw_filled_square((x) * (600/12),y,50)
     print(((x) * (600//12),y))
         
     red = (red - 20) % 250
@@ -52,8 +47,8 @@ while x < 1000:
     green = (blue + 20) % 255
 
 
-    display()
-    # draw_on_matrix()
+    # display()
+    picture.draw_on_matrix()
     # time.sleep(1)
 
     
