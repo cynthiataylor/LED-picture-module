f1d71cc54dee9a5cf701d7e8a540d20c72a78878
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-23 09:39:13 -0500

    save

--- picture.py
+++ picture.py
@@ -172,14 +172,13 @@ def new_picture(width, height):
     picture.new_picture(800, 600) #Creates a blank 800x600 picture
     ```
     """
-    global ROOT, FRAME, CANVAS, IMAGE, DRAW, MATRIX
-    global ROOT, FRAME, CANVAS, IMAGE, DRAW, MATRIX
+    global ROOT, FRAME, CANVAS, IMAGE, DRAW, matrix
     options = RGBMatrixOptions() #update
     options.rows = 64 #change this to board's width
     options.chain_length = 1
     options.parallel = 1
     options.hardware_mapping = 'adafruit-hat'
-    global ROOT, FRAME, CANVAS, IMAGE, DRAW, MATRIX
+    options.pixel_mapper_config = "U-mapper"
 
 
     if ROOT is None:
@@ -197,10 +196,11 @@ def new_picture(width, height):
         change_picture_size(width, height)
     IMAGE = Image.new("RGB", (width, height), color=(255, 255, 255))
     DRAW = ImageDraw.Draw(IMAGE)
+    matrix = RGBMatrix(options = options)
+
 
 def draw_on_matrix():
-    global MATRIX
-    MATRIX.SetImage(IMAGE, 0, 0)
+    matrix.SetImage(IMAGE, 0, 0)
 
 
 def save_picture(path):
