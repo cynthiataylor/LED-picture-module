64e14f8ac40b10e4d418435ea41cd0d3c742acdd
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-24 14:37:25 -0500

    save

--- Main/file_runner.py
+++ Main/file_runner.py
@@ -3,9 +3,9 @@ import subprocess
 
 def run(files):
     for file in files:
-        if file != 'file_runner.py':
-            print(file)
-            os.system(f"python3 {file}")
+        # if file != 'file_runner.py':
+        print(file)
+        os.system(f"python3 {file}")
 
 files = os.listdir()
 run(files)
