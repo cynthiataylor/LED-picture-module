aa052dd90c94e45968b415946f35916793377661
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-24 17:13:25 -0500

    save

--- Main/test/tetris.py
+++ Main/test/tetris.py
@@ -1,15 +1,82 @@
+# import picture
+# import random
+# import time
+
+# picture.new_picture(640,640)
+
+# # Set initial position 
+
+
+# picture.set_fill_color("black")
+# picture.set_outline_color("black")
+# picture.draw_filled_rectangle(0,0,640,640)
+
+
+# red = 205
+# green = 20
+# blue = 10
+# size = 2
+
+# x = 30
+# y = 30
+# picture.set_position(x,y)
+# runtime = 1
+# pos = 0
+# i = 0
+
+# taken_pos = []
+# runtime = 0
+# while True:
+# 	runtime += 1
+# 	taken_pos.append(x)
+# 	taken_pos.append(y)
+# 	picture.set_pen_width(size)
+# 	picture.set_fill_color(red,green,blue)
+# 	picture.set_outline_color(red,green,blue)
+# 	picture.draw_forward(i)
+# 	i += 1
+# 	if i == 30:
+# 		i = 0
+# 	if pos == 80:
+# 		# print(pos)
+# 		# picture.set_outline_color(red//2,green//3,blue//5)
+# 		pos = 0
+# 		x += 99
+# 		y += 100
+# 		picture.set_position(x,y)
+# 	if x % 100:
+# 		x = random.randint(0,500) if random.randint(0,600) not in taken_pos else random.randint(0,600)
+# 		y = random.randint(0,600) if random.randint(200,500) not in taken_pos else random.randint(0,600)
+# 	if y >= 600:
+# 		y = 0
+# 		x = 200
+# 	picture.display()
+# 	picture.rotate(random.randint(60,65))
+# 	time.sleep(0)
+# 	pos += 1
+
+# 	red = (red - 2) % 250
+# 	blue = (blue - 20) % 250
+# 	green = (blue + 20) % 255
+	
+# 	# print(taken_pos)
+
+
+
+
+
 import picture
 import random
 import time
 
-picture.new_picture(640,640)
+picture.new_picture(64,64)
 
 # Set initial position 
 
 
 picture.set_fill_color("black")
 picture.set_outline_color("black")
-picture.draw_filled_rectangle(0,0,640,640)
+picture.draw_filled_rectangle(0,0,64,64)
 
 
 red = 205
@@ -17,8 +84,8 @@ green = 20
 blue = 10
 size = 2
 
-x = 30
-y = 30
+x = 3
+y = 3
 picture.set_position(x,y)
 runtime = 1
 pos = 0
@@ -26,7 +93,7 @@ i = 0
 
 taken_pos = []
 runtime = 0
-while runtime > 3000:
+while True:
 	runtime += 1
 	taken_pos.append(x)
 	taken_pos.append(y)
@@ -35,22 +102,23 @@ while runtime > 3000:
 	picture.set_outline_color(red,green,blue)
 	picture.draw_forward(i)
 	i += 1
-	if i == 30:
+	if i == 4:
 		i = 0
-	if pos == 80:
+	if pos == 30:
 		# print(pos)
 		# picture.set_outline_color(red//2,green//3,blue//5)
 		pos = 0
-		x += 99
-		y += 100
+		x += 9
+		y += 10
 		picture.set_position(x,y)
-	if x % 100:
-		x = random.randint(0,500) if random.randint(0,600) not in taken_pos else random.randint(0,600)
-		y = random.randint(0,600) if random.randint(200,500) not in taken_pos else random.randint(0,600)
-	if y >= 600:
-		y = 0
-		x = 200
+	if x % 10:
+		x = random.randint(0,64) if random.randint(0,64) not in taken_pos else random.randint(0,64)
+		y = random.randint(0,64) if random.randint(0,64) not in taken_pos else random.randint(0,64)
+	# if y >= 6:
+	# 	y = 0
+	# 	x = 2
 	picture.display()
+	# picture.draw_on_matrix()
 	picture.rotate(random.randint(60,65))
 	time.sleep(0)
 	pos += 1
