dde28f00d3525b592b814d552ba534622ac64215
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-24 12:51:58 -0500

    save

--- practice/Beta/circling.py
+++ practice/Beta/circling.py
@@ -10,31 +10,31 @@ def circle(width,height):
     green = 200
     blue = 45
 
-    radius = 20
-    size = 1
+    radius = 2
+    size = 2
 
     computer = sys.argv[-1]
     while True:
-        picture.set_outline_color(red,green,blue)
-        if size ==10:
-            size = 2
-    
-            picture.set_pen_width(size)
-        size += 2
-        picture.draw_circle(width//2,height//2,radius)
+        picture.set_outline_color(red ,green, blue)
+        picture.set_pen_width(size)
+        size += 1
+        picture.draw_circle(width //2,height // 2,radius)
+        if size ==8:
+            size = 4
         radius += 2
 
         if computer == "L":
             picture.display()
         elif computer == "D":
             picture.draw_on_matrix()
-            time.sleep(0.05)
+            time.sleep(float(sys.argv[2]))
 
         if radius >= 40:
             radius = 4
             picture.set_fill_color("black")
             picture.set_outline_color("black")
             picture.draw_filled_rectangle(0,0,width,height)
+            
     
 
         red = (red - 20) % 250
