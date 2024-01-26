2b958b0c299f4a88414798ec7cb9c5a63486d442
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-26 14:47:31 -0500

    save

--- practice/Beta/crawling.py
+++ practice/Beta/crawling.py
@@ -1,5 +1,5 @@
 import sys
-import Main.picture as picture
+import picture
 import time
 
 picture.new_picture(int(sys.argv[1]),int(sys.argv[1]))
@@ -9,31 +9,54 @@ picture.draw_filled_rectangle(0,0,int(sys.argv[1]),int(sys.argv[1]))
 red = 255
 green = 200
 blue = 45
-sec = 1
 
+sec = 0.005
+move = 100
+top = 100
+bottom = 0
+right = 5
+left = 5
+back = 64
+x = 5
+y = 100
 while True:
-    for i in range(200):
+    for i in range(2000):
         picture.set_fill_color(red,green,blue)
         picture.set_outline_color(red,green,blue)
         picture.set_pen_width(20)
         #picture.set_pen_x(34)
         #picture.set_pen_y(34)
-        picture.set_position(int(sys.argv[1])//4,int(sys.argv[1])//4)
+        picture.set_position(int(sys.argv[1])//2,int(sys.argv[1])-30)
         picture.draw_forward(20)
         picture.draw_on_matrix()
         time.sleep(sec)
         picture.set_fill_color("black")
         picture.set_outline_color("black")
         picture.draw_filled_rectangle(0,0,int(sys.argv[1]),int(sys.argv[1]))
-        picture.rotate(10)
+        picture.rotate(50)
 
-        # if i == 100:
-        #     picture.draw_filled_square(int(sys.argv[1])//2,int(sys.argv[1])//2, 10)
-        # if i == 199:
-        #     picture.set_fill_color("black")
-        #     picture.draw_filled_rectangle(0,0,int(sys.argv[1]),int(sys.argv[1]))
-        #     picture.rotate(150)
-
+        picture.set_outline_color(red,green,blue)
+        picture.draw_text(move,25,"Welcome to computer science department",30)
+        move -= 1
+        if move == -100:
+                move = 100
+        picture.draw_filled_circle(x,y,4)
+        
+        
+        if y <= 10:
+                y = right
+                x = left
+                left +=1
+        if left >=64:
+                y = bottom
+                
+                x = 64
+                bottom +=1
+        y -=1
+        
+        if bottom <= 64:
+                print(bottom)
+                
             
         red = (red - 20) % 250
         blue = (blue - 20) % 250
