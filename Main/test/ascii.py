e90299b5bf18f2c83a15f1d860b0047376fe77e1
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-28 17:09:00 -0500

    save

---
+++ Main/test/ascii.py
@@ -0,0 +1,3 @@
+name = '\n'.join([''.join([('computerscience'[(x-y) % len('computerscience')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else' ') for x in range(-30, 30)]) for y in range(30, -30, -1)])
+
+print(name)
