52e78df9d6b648967ae79e86bd2e3b9ff902a348
tharcissentirandekura <tntirand@oberlin.edu>
2024-02-04 17:56:56 -0500

    SAVE

--- samples/rotatingblock.py
+++ samples/rotatingblock.py
@@ -10,29 +10,29 @@ red = 255
 green = 200
 blue = 45
 
-runtime = 800
-while runtime > 0: 
+runtime = 10
+# while runtime > 0: 
     
-    for i in range(100):
-        runtime -=1
-        picture.set_fill_color(red,green,blue)
-        picture.set_outline_color(red,green,blue)
-        picture.set_pen_width(40)
-        # picture.set_pen_x(300)
-        # picture.set_pen_y(300)
-        picture.set_position(128//2,128//2)
-        picture.draw_forward(i * 2)
-        picture.draw_on_matrix()
-        time.sleep(1)
-        picture.draw_forward(460)
-        picture.rotate(10)
+for i in range(100):
+    runtime -=1
+    picture.set_fill_color(red,green,blue)
+    picture.set_outline_color(red,green,blue)
+    picture.set_pen_width(40)
+    # picture.set_pen_x(300)
+    # picture.set_pen_y(300)
+    picture.set_position(128//2,128//2)
+    picture.draw_forward(i * 2)
+    picture.draw_on_matrix()
+    time.sleep(1)
+    picture.draw_forward(460)
+    picture.rotate(10)
 
-        picture.set_fill_color("black")
-        picture.set_outline_color("black")
-        picture.draw_filled_rectangle(0,0,128,128)
-       
+    picture.set_fill_color("black")
+    picture.set_outline_color("black")
+    picture.draw_filled_rectangle(0,0,128,128)
+    
 
-            
-        red = (red - 20) % 250
-        blue = (blue - 20) % 250
-        green = (blue + 20) % 255
+        
+    red = (red - 20) % 250
+    blue = (blue - 20) % 250
+    green = (blue + 20) % 255
