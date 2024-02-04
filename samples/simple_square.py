26fb3fdc5ba44bea13d9f479c6e9ed767cff59cd
tharcissentirandekura <tntirand@oberlin.edu>
2024-02-04 16:39:58 -0500

    save

--- samples/simple_square.py
+++ samples/simple_square.py
@@ -4,5 +4,7 @@ picture.new_picture(128,128)
 picture.set_fill_color("green")
 
 picture.draw_filled_square(64,64,30)
-picture.draw_on_matrix()
-picture.delay(100)
+while True:
+    picture.draw_on_matrix()
+
+
