ca3622bd8a505697c6fbe9dedfe52814aa958e78
tharcissentirandekura <tntirand@oberlin.edu>
2024-02-04 18:08:25 -0500

    save

--- samples/ScrollingSquare.py
+++ samples/ScrollingSquare.py
@@ -20,43 +20,41 @@ up = 120
 positions = []
 counter = 0
 
-runtime = 100
-while True:
-    for t in range(4):
-        runtime -=1
-        x+= 1
 
-        picture.set_fill_color(red,green,blue)
+for t in range(40):
+    x+= 1
+
+    picture.set_fill_color(red,green,blue)
+    
+    picture.draw_filled_square(x,y,10)
+    picture.draw_filled_square(y,x,10)
+    picture.draw_filled_square(up,down,10)
+    picture.draw_filled_square(down,up,10)
+    if counter % 50 == 0:
+        picture.draw_filled_square(random.randint(3,120),random.randint(3,120),10)
+    
+    
+    if x >=128:
+        x = 0
+        y += 4
+    if y >= 128:
+        y = 0
+        x = 0
         
-        picture.draw_filled_square(x,y,10)
-        picture.draw_filled_square(y,x,10)
-        picture.draw_filled_square(up,down,10)
-        picture.draw_filled_square(down,up,10)
-        if counter % 50 == 0:
-            picture.draw_filled_square(random.randint(3,120),random.randint(3,120),10)
-        
-        
-        if x >=128:
-            x = 0
-            y += 4
-        if y >= 128:
-            y = 0
-            x = 0
-            
-        if up <=4:
-            up = 120
-            down -= 2
-        if down <=4:
-            down = 120
-            up = 120
-        counter += 1
-        #down -= 1
-        up -=2
-  
-        picture.draw_on_matrix()
+    if up <=4:
+        up = 120
+        down -= 2
+    if down <=4:
+        down = 120
+        up = 120
+    counter += 1
+    #down -= 1
+    up -=2
+
+    picture.draw_on_matrix()
 
 
-        blue = (blue - 4) % 255 
-        #red = (red - y) % 255
-        green = (green //(x + 1))%255
-        
+    blue = (blue - 4) % 255 
+    #red = (red - y) % 255
+    green = (green //(x + 1))%255
+    
